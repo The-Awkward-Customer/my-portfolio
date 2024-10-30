c5feb3873625b5eebfcfcaf9a7ebbd25cb5caf61
PETER ABBOTT <abbottsayshi@gmail.com>
2024-10-30 20:21:49 +0100

    added linking and global css

---
+++ src/app/about-me/page.jsx
@@ -0,0 +1,15 @@
+// pages/about.js
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Hello, I'm Peter Abbott! I'm a passionate software developer focused on creating delightful digital experiences. Welcome to my portfolio!</p>
+      <p>I love working with web technologies, building interactive interfaces, and working on exciting projects like my current BumbleBoogie game.</p>
+    </div>
+  );
+};
+
+export default About;
