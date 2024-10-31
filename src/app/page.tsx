23d18ea255c1c24b67580b019c87486e8b75fd01
PETER ABBOTT <abbottsayshi@gmail.com>
2024-10-31 15:48:00 +0000

    added Top-Nav Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import TopNav from "@/components/top-nav/top-nav";
 
 export default function Home() {
+
+const links = [
+  {label: 'home', href: '/'},
+  {label: 'About me', href: '/about-me'}
+]
+
+
   return (
     <div>
       <h1>Peter Abbott</h1>
       <p>Hello World!</p>
+
+      <TopNav title="Peter Abbott" links={links}/>
 
       <nav>
         <Link href={"/about-me"}>About me</Link>
